236a6f00bb59e1863c5a0e546599faa21af149f5
IanMGF <ianmarcosgomesefreitas@gmail.com>
2023-12-09 09:50:13 -0300

    javadoc RelatorioUseCases

--- logic/RelatorioUseCases.java
+++ logic/RelatorioUseCases.java
@@ -1,15 +1,24 @@
 package logic;
 
+/**
+ * A classe RelatorioUseCases é responsável por gerar relatórios com informações sobre clientes, veículos e locações/reservas.
+ */
 public class RelatorioUseCases {
-    
+
+    /**
+     * Gera um relatório com informações sobre clientes, veículos e locações/reservas.
+     * @param clientes Se true, inclui informações sobre clientes no relatório.
+     * @param veiculo Se true, inclui informações sobre veículos no relatório.
+     * @param loc_res Se true, inclui informações sobre locações/reservas no relatório.
+     * @return O relatório gerado.
+     */
     public static String getRelatorio(boolean clientes, boolean veiculo, boolean loc_res){
         String clientesStr = "", veiculoStr = "", loc_resStr = "";
 
-
         if(clientes){
-            
+            // TODO: adicionar informações sobre clientes
         }
-        
+
         if(veiculo){
             IVeiculo[] veiculos = Frota.getVeiculos();
             veiculoStr = "Relatorios de Veículos: \n";
@@ -21,7 +30,7 @@ public class RelatorioUseCases {
         }
 
         if(loc_res){
-            
+            // TODO: adicionar informações sobre locações/reservas
         }
 
         String relatorio = (clientesStr + veiculoStr + loc_resStr);
@@ -29,4 +38,3 @@ public class RelatorioUseCases {
     }
 
 }
-
