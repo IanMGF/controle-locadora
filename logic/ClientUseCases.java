11f8a6fd7d91ad609c95c4d50534b96c44569a4b
IanMGF <ianmarcosgomesefreitas@gmail.com>
2023-12-08 23:49:10 -0300

    javadoc ClientUseCases

--- logic/ClientUseCases.java
+++ logic/ClientUseCases.java
@@ -1,3 +1,6 @@
+/**
+ * Esta classe contém métodos para gerenciar casos de uso do cliente.
+ */
 package logic;
 
 import exceptions.CPFAlreadyRegisteredException;
@@ -8,7 +11,13 @@ import java.time.LocalDate;
 import java.util.Date;
 
 public class ClientUseCases {
-	public static ICliente encontrarPorCPF(String cpf){
+	/**
+	 * Procura um cliente pelo CPF e retorna o cliente encontrado ou null se nenhum cliente for encontrado.
+	 *
+	 * @param cpf O número do CPF a ser procurado.
+	 * @return O cliente encontrado ou null se nenhum cliente for encontrado.
+	 */
+	public static ICliente encontrarPorCPF(String cpf) {
 		return ClientDatabase.getClientsCopy()
 				.stream()
 				.filter(client -> client.getCPF().equals(cpf))
@@ -16,25 +25,43 @@ public class ClientUseCases {
 				.orElse(null);
 	}
 
-	// Valida as informações, caso estejam erradas, causa um erro
+	/**
+	 * Valida as informações fornecidas e lança uma exceção se as informações forem inválidas.
+	 *
+	 * @param cpf O número do CPF a ser validado.
+	 * @param dataDeNascimento A data de nascimento a ser validada.
+	 * @param email O e-mail a ser validado.
+	 * @throws CPFAlreadyRegisteredException Lançada quando o CPF já está registrado.
+	 * @throws InvalidCPFException Lançada quando o CPF é inválido.
+	 * @throws NotOldEnoughException Lançada quando o usuário não tem idade suficiente (18 anos).
+	 */
 	public static void validarInfo(String cpf, LocalDate dataDeNascimento, String email)
-			throws CPFAlreadyRegisteredException, InvalidCPFException, NotOldEnoughException
-	{
-		if(encontrarPorCPF(cpf) != null){
+			throws CPFAlreadyRegisteredException, InvalidCPFException, NotOldEnoughException {
+		if (encontrarPorCPF(cpf) != null) {
 			throw new CPFAlreadyRegisteredException(cpf);
 		}
 
-		if(!validarCPF(cpf)){
+		if (!validarCPF(cpf)) {
 			throw new InvalidCPFException(cpf);
 		}
 
 		LocalDate localDate = LocalDate.now();
 		boolean isOver18 = dataDeNascimento.plusYears(18).isBefore(localDate);
-		if(!isOver18){
+		if (!isOver18) {
 			throw new NotOldEnoughException();
 		}
 	}
 
+	/**
+	 * Registra um novo cliente com as informações fornecidas.
+	 *
+	 * @param nomeCompleto O nome completo do cliente.
+	 * @param cpf O número do CPF do cliente.
+	 * @param nascimento A data de nascimento do cliente.
+	 * @param email O e-mail do cliente.
+	 * @param celular O número do celular do cliente.
+	 * @param contato As informações de contato do cliente.
+	 */
 	public static void registrarNovoCliente(
 			String nomeCompleto,
 			String cpf,
@@ -42,7 +69,7 @@ public class ClientUseCases {
 			String email,
 			String celular,
 			String contato
-		){
+	) {
 		ICliente client = new Cliente(
 				nomeCompleto,
 				cpf,
@@ -50,22 +77,28 @@ public class ClientUseCases {
 				email,
 				celular,
 				contato
-			);
+		);
 
 		ClientDatabase.add(client);
 		ClientDatabase.saveToFile();
 	}
 
+	/**
+	 * Verifica se o CPF fornecido é válido.
+	 *
+	 * @param cpf O número do CPF a ser validado.
+	 * @return True se o CPF for válido, false caso contrário.
+	 */
 	private static boolean validarCPF(String cpf) {
-		// Remover caracteres não numéricos
+		// Remove caracteres não numéricos
 		cpf = cpf.replaceAll("[^0-9]", "");
 
-		// Verificar se o CPF tem 11 dígitos
+		// Verifica se o CPF tem 11 dígitos
 		if (cpf.length() != 11) {
 			return false;
 		}
 
-		// Calcular o primeiro dígito verificador
+		// Calcula o primeiro dígito verificador
 		int soma = 0;
 		for (int i = 0; i < 9; i++) {
 			soma += Character.getNumericValue(cpf.charAt(i)) * (10 - i);
@@ -73,7 +106,7 @@ public class ClientUseCases {
 		int digito1 = 11 - (soma % 11);
 		digito1 = (digito1 > 9) ? 0 : digito1;
 
-		// Calcular o segundo dígito verificador
+		// Calcula o segundo dígito verificador
 		soma = 0;
 		for (int i = 0; i < 10; i++) {
 			soma += Character.getNumericValue(cpf.charAt(i)) * (11 - i);
@@ -81,9 +114,9 @@ public class ClientUseCases {
 		int digito2 = 11 - (soma % 11);
 		digito2 = (digito2 > 9) ? 0 : digito2;
 
-		// Verificar se os dígitos verificadores estão corretos
+		// Verifica se os dígitos verificadores estão corretos
 		boolean primeiroDigitoCorreto = Character.getNumericValue(cpf.charAt(9)) == digito1;
 		boolean segundoDigitoCorreto = Character.getNumericValue(cpf.charAt(10)) == digito2;
-		return  primeiroDigitoCorreto && segundoDigitoCorreto;
+		return primeiroDigitoCorreto && segundoDigitoCorreto;
 	}
 }
