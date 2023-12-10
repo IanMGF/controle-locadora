8d04306513fb17d2586f04809a697ba7e67ec45f
Neto Moraes <jomoraesneto@gmail.com>
2023-12-10 19:56:09 -0300

    finished relatorio

--- logic/RelatorioUseCases.java
+++ logic/RelatorioUseCases.java
@@ -20,35 +20,43 @@ public class RelatorioUseCases {
 
         if(clientes){
             List<ICliente> clientList = ClientDatabase.getClientsCopy();
-            clientesStr = "Relatorios de Clientes (" + clientList.size() + " clientes registrados): \n";
             if(clientList.size() == 0){
                 throw new NotEnoughDataException();
             }
+            clientesStr = "Relatorios de Clientes (" + clientList.size() + " clientes registrados): \n";
 
             for(ICliente client : clientList){
                 clientesStr = (clientesStr + "Nome: " + client.getNomeCompleto() + " | CPF: " + client.getCPF() + " | Data de nascimento: " + client.getNascimento() 
                                 + " | Email: " + client.getEmail() + " | Celular: " + client.getCelular() + "\n");
             }
+            clientesStr += "\n";
         }
 
         if(veiculo){
             IVeiculo[] veiculos = Frota.getVeiculos();
-            veiculoStr = "Relatorios de Veículos(" + veiculos.length + " veículos na frota): \n";
             if(veiculos.length == 0){
                 throw new NotEnoughDataException();
             }
+            veiculoStr = "Relatorios de Veículos(" + veiculos.length + " veículos na frota): \n";
             for (IVeiculo iVeiculo : veiculos) {
-                veiculoStr = (veiculoStr + "Placa: " + iVeiculo.getPlaca() + "| Marca: " + iVeiculo.getMarca() + "| Modelo: " + iVeiculo.getModelo() + "| Cor: " +
-                        iVeiculo.getCor() + "| Ano: " + iVeiculo.getAno() + "| Grupo: " + iVeiculo.getGrupo() + "| Status: " + iVeiculo.getStatus() + "\n");
+                veiculoStr = (veiculoStr + "Placa: " + iVeiculo.getPlaca() + " | Marca: " + iVeiculo.getMarca() + " | Modelo: " + iVeiculo.getModelo() + " | Cor: " +
+                        iVeiculo.getCor() + " | Ano: " + iVeiculo.getAno() + " | Grupo: " + iVeiculo.getGrupo() + " | Status: " + iVeiculo.getStatus() + "\n");
             }
+            veiculoStr += "\n";
         }
 
         if(loc_res){
-            // Se não houverem dados de locação, lançar NotEnoughDataException
-//            if(clientList.size() == 0){
-//                throw NotEnoughDataException();
-//            }
-            // TODO: adicionar informações sobre locações/reservas
+            List<IReserva> reservaList = ReservaDatabase.getReservasCopy();
+            if(reservaList.size() == 0){
+                throw new NotEnoughDataException();
+            }
+            loc_resStr = "Relatorios de Reservas (" + reservaList.size() + " reservas registradas): \n";  
+            
+            for(IReserva reserva : reservaList){
+                loc_resStr = (loc_resStr + "Identificador: " + reserva.getCodigo() + " | CPF-Cliente: " + reserva.getCPF()  + " | Data-Retirada: " + reserva.getDataRetirada() + 
+                            " | Data-Devolucao: " + reserva.getDataDevolucao() + " | Valor-Locacao: " + reserva.getValor() + " | Status: " + reserva.getStatus() + "\n");
+            }
+            loc_resStr += "\n";
         }
 
         String relatorio = (clientesStr + veiculoStr + loc_resStr);
